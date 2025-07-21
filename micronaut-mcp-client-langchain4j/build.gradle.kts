c478d00cb3c13f673cd1396361eb650e5eb1a770
Sergio del Amo <sergio.delamo@softamo.com>
2025-07-21 12:19:37 +0200

    Add MCP Client Langchain4j Module
    This pullrequest adds a MCP Client Langchain4j Module. I will use this module in a test in a separate PR.

--- micronaut-mcp-client-langchain4j/build.gradle.kts
+++ micronaut-mcp-client-langchain4j/build.gradle.kts
@@ -1,3 +1,17 @@
+import io.micronaut.build.TestFramework
 plugins {
     id("io.micronaut.build.internal.mcp-module")
 }
+dependencies {
+    implementation(platform("dev.langchain4j:langchain4j-bom:${mnLangchain4j.versions.langchain4j.asProvider().get()}"))
+    api(libs.langchain4j.mcp)
+    testAnnotationProcessor(mn.micronaut.inject.java)
+}
+micronautBuild {
+    testFramework = TestFramework.JUNIT5
+}
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
