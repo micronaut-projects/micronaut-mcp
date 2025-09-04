0ea1d447a31c037c528885048a2c6ee8d3a7e738
Sergio del Amo <sergio.delamo@softamo.com>
2025-09-04 16:51:24 +0200

    Add MCP EmbeddedApplication for STDIO transport (#34)
    * Adds a MCP EmbeddedApplication for STDIO transport
    
    This alllow to create an mcp server without any additional runtime (e.g. no netty).
    
    We do a similar thing for Messaging applications:
    
    https://raw.githubusercontent.com/micronaut-projects/micronaut-core/refs/heads/4.10.x/messaging/src/main/java/io/micronaut/messaging/MessagingApplication.java
    
    * Update micronaut-mcp-server-java-sdk/src/main/java/io/micronaut/mcp/server/stdio/McpStdioApplication.java
    
    * Update micronaut-mcp-server-java-sdk/src/main/java/io/micronaut/mcp/server/stdio/McpStdioApplication.java

---
+++ micronaut-mcp-server-java-sdk/src/test/java/io/micronaut/mcp/server/stdio/McpStdioApplicationTest.java
@@ -0,0 +1,25 @@
+package io.micronaut.mcp.server.stdio;
+
+import io.micronaut.context.ApplicationContext;
+import io.micronaut.mcp.conf.McpServerConfiguration;
+import io.micronaut.runtime.ApplicationConfiguration;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class McpStdioApplicationTest {
+
+    @Test
+    void testMcpStdioApplicationCanStartAndStopWithoutExceptions() {
+
+        try (ApplicationContext ctx = ApplicationContext.run(Map.of("micronaut.mcp.server.transport", "STDIO"))) {
+            var app = new McpStdioApplication(ctx, ctx.getBean(ApplicationConfiguration.class), ctx.getBean(McpServerConfiguration.class));
+            assertDoesNotThrow(app::start);
+            assertTrue(app.isRunning());
+            assertEquals("STDIO sync", app.getDescription());
+            assertDoesNotThrow(app::close);
+        }
+    }
+}
