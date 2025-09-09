0692655511f073f3c2fd366c0e8bd3df0e1b0785
Sergio del Amo <sergio.delamo@softamo.com>
2025-09-09 09:54:30 +0200

    Add GraalVM Reflection Configuration
    This PR adds a test project for the MCP Server tools Weather demo in https://modelcontextprotocol.io/docs/develop/build-server
    
    It adds reflect-config.json configuration for every Java record in https://github.com/modelcontextprotocol/java-sdk/blob/main/mcp/src/main/java/io/modelcontextprotocol/spec/McpSchema.java to make the native tests pass.

---
+++ test-suite-graal/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.gradle.kotlin.dsl.testImplementation
+import org.gradle.kotlin.dsl.testRuntimeOnly
+
+plugins {
+    id("io.micronaut.application") version "4.5.4"
+}
+version = "0.1"
+group = "example.micronaut"
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation(mn.micronaut.jackson.databind)
+    implementation(projects.micronautMcpServerJavaSdk)
+    annotationProcessor(mnJsonSchema.micronaut.json.schema.processor)
+    implementation(mnJsonSchema.micronaut.json.schema.annotations)
+    implementation(mn.micronaut.http.client)
+    runtimeOnly(mnLogging.logback.classic)
+    testImplementation(libs.jsonassert)
+}
+application {
+    mainClass = "example.micronaut.Application"
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("21")
+    targetCompatibility = JavaVersion.toVersion("21")
+}
+micronaut {
+    version(libs.versions.micronaut.platform.get())
+    runtime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+        annotations("example.micronaut.*")
+    }
+}
