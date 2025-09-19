c8aa7fff4c7195d6839ed8ff2f654b43a6a4ddba
Sergio del Amo <sergio.delamo@softamo.com>
2025-09-19 12:59:12 +0200

    Use serde (#53)
    * Revert "remove serde"
    
    This reverts commit 9b68fc357767a414786f804b34a9018ba3b36051.
    
    * use class import
    
    * Fix incorrect `JsonTypeInfo`
    
    * remove supplier
    
    ---------
    
    Co-authored-by: Denis Stepanov <denis.s.stepanov@oracle.com>

--- micronaut-mcp-server-java-sdk/build.gradle.kts
+++ micronaut-mcp-server-java-sdk/build.gradle.kts
@@ -4,8 +4,10 @@ plugins {
 }
 dependencies {
     api(mn.micronaut.json.core)
+    implementation(mnSerde.micronaut.serde.api)
     compileOnly(mn.jackson.databind)
-    testImplementation(mn.micronaut.jackson.databind)
+    annotationProcessor(mnSerde.micronaut.serde.processor)
+    testImplementation(mnSerde.micronaut.serde.jackson)
 
     api(libs.managed.mcp.java.sdk)
     api(projects.micronautMcp)
