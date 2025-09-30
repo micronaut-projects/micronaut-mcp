d52f9b0cc617c3d57d907f57e4e66439437b4807
Sergio del Amo <sergio.delamo@softamo.com>
2025-09-30 11:58:43 +0200

    write tests calling tool

--- micronaut-mcp-client-java-sdk/src/test/java/io/micronaut/mcp/client/javasdk/McpSyncClientTest.java
+++ micronaut-mcp-client-java-sdk/src/test/java/io/micronaut/mcp/client/javasdk/McpSyncClientTest.java
@@ -1,25 +1,47 @@
 package io.micronaut.mcp.client.javasdk;
 
+import example.micronaut.moon.MoonPhase;
+import example.micronaut.moon.MoonPhaseEmoji;
 import io.micronaut.context.annotation.Property;
 import io.micronaut.core.util.StringUtils;
+import io.micronaut.json.JsonMapper;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import io.modelcontextprotocol.client.McpSyncClient;
 import io.modelcontextprotocol.spec.McpSchema;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @MicronautTest
 class McpSyncClientTest {
     @Test
-    void testInjectMcpSyncClient(McpSyncClient client) {
+    void testInjectMcpSyncClient(McpSyncClient client, JsonMapper jsonMapper) throws IOException {
         assertDoesNotThrow(client::initialize);
         McpSchema.ListToolsResult listToolsResult = assertDoesNotThrow(() -> client.listTools());
         List<String> toolNames = listToolsResult.tools().stream().map(McpSchema.Tool::name).toList();
         assertTrue(toolNames.stream().anyMatch(name -> name.equals("current-moon-phase")));
         assertTrue(toolNames.stream().anyMatch(name -> name.equals("moon-phase-at-date")));
+
+        McpSchema.CallToolResult callToolResult = assertDoesNotThrow(() -> client.callTool(McpSchema.CallToolRequest.builder()
+            .name("moon-phase-at-date")
+            .arguments(Map.of("date", "1982-10-28"))
+            .build()));
+        assertFalse(callToolResult.content().isEmpty());
+        assertTrue(callToolResult.content().get(0) instanceof McpSchema.TextContent);
+        McpSchema.TextContent textContent = (McpSchema.TextContent) callToolResult.content().get(0);
+        assertEquals("""
+    {"phase":"WAXING_GIBBOUS","emoji":"\\uD83C\\uDF14"}""", textContent.text());
+        String structuredContentJson = jsonMapper.writeValueAsString(callToolResult.structuredContent());
+        MoonPhaseEmoji moonPhaseEmoji = jsonMapper.readValue(structuredContentJson, MoonPhaseEmoji.class);
+        assertNotNull(moonPhaseEmoji);
+        assertEquals(moonPhaseEmoji, new MoonPhaseEmoji(MoonPhase.WAXING_GIBBOUS, "\uD83C\uDF14"));
     }
 }
--- micronaut-mcp-client-langchain4j/src/test/java/io/micronaut/mcp/client/langchain4j/http/McpClientTest.java
+++ micronaut-mcp-client-langchain4j/src/test/java/io/micronaut/mcp/client/langchain4j/http/McpClientTest.java
@@ -1,15 +1,25 @@
 package io.micronaut.mcp.client.langchain4j.http;
 
+import dev.langchain4j.agent.tool.ToolExecutionRequest;
 import dev.langchain4j.agent.tool.ToolSpecification;
 import dev.langchain4j.mcp.client.McpClient;
+import example.micronaut.moon.MoonPhase;
+import example.micronaut.moon.MoonPhaseEmoji;
 import io.micronaut.context.annotation.Property;
 import io.micronaut.core.util.StringUtils;
+import io.micronaut.json.JsonMapper;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.modelcontextprotocol.spec.McpSchema;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Property(name = "micronaut.mcp.server.info.name", value = "mcp-server")
@@ -21,10 +31,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @MicronautTest
 class McpClientTest {
     @Test
-    void testInjectMcpClient(McpClient client) {
+    void testInjectMcpClient(McpClient client, JsonMapper jsonMapper) throws IOException {
         List<ToolSpecification> listToolsResult = assertDoesNotThrow(client::listTools);
         List<String> toolNames = listToolsResult.stream().map(ToolSpecification::name).toList();
         assertTrue(toolNames.stream().anyMatch(name -> name.equals("current-moon-phase")));
         assertTrue(toolNames.stream().anyMatch(name -> name.equals("moon-phase-at-date")));
+
+        String arguments = jsonMapper.writeValueAsString(Map.of("date", "1982-10-28"));
+        String json = assertDoesNotThrow(() -> client.executeTool(ToolExecutionRequest.builder()
+            .name("moon-phase-at-date").arguments(arguments).build()));
+        assertNotNull(json);
+        MoonPhaseEmoji moonPhaseEmoji = jsonMapper.readValue(json, MoonPhaseEmoji.class);
+        assertNotNull(moonPhaseEmoji);
+        assertEquals(moonPhaseEmoji, new MoonPhaseEmoji(MoonPhase.WAXING_GIBBOUS, "\uD83C\uDF14"));
     }
 }
