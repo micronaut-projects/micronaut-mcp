ba177500467146892faf7e7ae3f417adc7e37be4
Sergio del Amo <sergio.delamo@softamo.com>
2025-10-03 17:04:22 +0200

    doc: document that Server to client communication via SSE streams is not yet supported (#73)

--- micronaut-mcp-server-java-sdk/src/test/java/io/micronaut/mcp/server/stateless/sync/tools/StatelessSyncToolsTest.java
+++ micronaut-mcp-server-java-sdk/src/test/java/io/micronaut/mcp/server/stateless/sync/tools/StatelessSyncToolsTest.java
@@ -2,6 +2,8 @@ package io.micronaut.mcp.server.stateless.sync.tools;
 
 import io.micronaut.context.annotation.Property;
 import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.MediaType;
 import io.micronaut.http.client.BlockingHttpClient;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
@@ -15,6 +17,7 @@ import static io.micronaut.mcp.server.utils.JsonRpcMessages.EXPECTED_TOOLS_LIST;
 import static io.micronaut.mcp.server.utils.JsonRpcMessages.TOOLS_CALL;
 import static io.micronaut.mcp.server.utils.JsonRpcMessages.TOOLS_LIST;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Property(name = "micronaut.mcp.server.transport", value = "HTTP")
 @Property(name = "spec.name", value = "ToolsTest")
@@ -25,7 +28,9 @@ class StatelessSyncToolsTest {
     void toolsList(@Client("/") HttpClient httpClient) throws JSONException {
         BlockingHttpClient client = httpClient.toBlocking();
         HttpRequest<?> req = HttpRequest.POST("/mcp", TOOLS_LIST);
-        String result = assertDoesNotThrow(() -> client.retrieve(req));
+        HttpResponse<String> resp = assertDoesNotThrow(() -> client.exchange(req, String.class));
+        assertEquals(MediaType.APPLICATION_JSON_TYPE, resp.getContentType().get());
+        String result = resp.getBody(String.class).get();
         JSONAssert.assertEquals(EXPECTED_TOOLS_LIST, result, true);
     }
 
